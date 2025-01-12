fea27bb31c5bc2a72ffbf7b1239d3fc258c47835
mattgauntlet199 <38744915-mattgauntlet199@users.noreply.replit.com>
2025-01-12 14:00:44 +0000

    Checkpoint after starting plan

--- server/index.ts
+++ server/index.ts
@@ -37,7 +37,8 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  const server = registerRoutes(app);
+  // Await the server creation
+  const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
@@ -47,9 +48,7 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
+  // Setup vite after we have the actual server instance
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
