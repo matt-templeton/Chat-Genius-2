22b8f898b5af9a3905ee2b7f3a0e5733830b5653
Matt Templeton <MattTempleton1@gmail.com>
2025-01-13 14:08:50 +0000

    BLAH

--- jest.config.ts
+++ jest.config.ts
@@ -3,25 +3,17 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
   setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
-  // transform: {
-  //   '^.+\\.tsx?$': ['ts-jest', {
-  //     useESM: true,
-  //     tsconfig: {
-  //       module: 'ESNext',
-  //       moduleResolution: 'bundler'
-  //     }
-  //   }]
-  // },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/client/src/$1",
     "^@db/(.*)$": "<rootDir>/db/$1",
-    "^@db$": "<rootDir>/db/index.ts",
+    "^@db": "<rootDir>/db/index.ts",
   },
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
-  transform: { "^.+\\.tsx?$": "ts-jest" },
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
 
 export default config;
